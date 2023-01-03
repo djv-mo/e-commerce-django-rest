15d5d35fd090a2b36bd9fd5ff7c3780c29b9c046
mohamed Ali <solutiond700@gmail.com>
2023-01-03 13:34:38 +0200

    adding product, category and ProductImage models

--- shop/settings.py
+++ shop/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     # my apps
     'users',
+    'store',
 ]
 
 MIDDLEWARE = [
--- store/models.py
+++ store/models.py
@@ -1,3 +1,43 @@
+from uuid import uuid4
+from autoslug import AutoSlugField
+from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    slug = AutoSlugField(unique=True, always_update=False,
+                         populate_from="title")
+    description = models.TextField(null=True, blank=True)
+    unit_price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(1)])
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
+    last_update = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, related_name='products')
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(
+        upload_to='store/images')
