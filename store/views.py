c9ebd31841bf921516dcd87622dae2634c04eace
mohamed Ali <solutiond700@gmail.com>
2023-01-05 20:40:27 +0200

    updating category views destroy

--- store/views.py
+++ store/views.py
@@ -21,8 +21,8 @@ class CategoryViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
 
     def destroy(self, request, *args, **kwargs):
-        if Product.objects.filter(collection_id=kwargs['pk']):
-            return Response({'error': 'Collection cannot be deleted because it includes one or more products.'},
+        if Product.objects.filter(category_id=kwargs['pk']):
+            return Response({'error': 'Category cannot be deleted because it includes one or more products.'},
                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
         return super().destroy(request, *args, **kwargs)
