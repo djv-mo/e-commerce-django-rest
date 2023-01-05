2c5c5e4a47eb1c4c4780d4e2103ee20ea6196b52
mohamed Ali <solutiond700@gmail.com>
2023-01-05 22:06:24 +0200

    check if the cart items quentity is more than inventory of the product

--- store/serializers.py
+++ store/serializers.py
@@ -75,11 +75,19 @@ class AddCartItemSerializer(serializers.ModelSerializer):
         cart_id = self.context['cart_id']
         product_id = self.validated_data['product_id']
         quantity = self.validated_data['quantity']
+        selected_product = Product.objects.get(pk=product_id)
+        if quantity > selected_product.inventory:
+            raise serializers.ValidationError(
+                "You have entered a quantity that is more than the product's availabe quantity")
 
         try:
             cart_item = CartItem.objects.get(
                 cart_id=cart_id, product_id=product_id)
-            cart_item.quantity += quantity
+            if cart_item.quantity >= selected_product.inventory:
+                raise serializers.ValidationError(
+                    "You have entered a quantity that is more than the product's availabe quantity")
+            else:
+                cart_item.quantity += quantity
             cart_item.save()
             self.instance = cart_item
         except CartItem.DoesNotExist:
